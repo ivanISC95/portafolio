d20f6340f113e17c3c32ac350abeeb1a321e77a6
Jose Ivan Perez Ugalde <ivan2295isc@gmail.com>
2022-06-21 15:31:24 -0500

    React-router-dom agregado

--- portafolio/src/components/principal/App.js
+++ portafolio/src/components/principal/App.js
@@ -1,19 +1,22 @@
-import Home from "./Home/Home";
 import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import '../../Styles/Style.css';
+import Home from "../Home/Home";
+import About from '../About/About';
+import Contact from '../Contact/Contact';
 export default function App() {
     return (
         <div>
-            <div>
+            <Router>
                 <Navbar className="NavBar" expand="lg" variant="dark">
                     <Container >
-                        <Navbar.Brand href="#home">Ivan/Portafolio</Navbar.Brand>
+                        <Navbar.Brand as={Link} to="/">Ivan/Portafolio</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto">
-                                <Nav.Link href="#home">Home</Nav.Link>
-                                <Nav.Link href="#link">About</Nav.Link>
-                                <Nav.Link href="#link">Contact</Nav.Link>
+                                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                                <Nav.Link as={Link} to="/About">About</Nav.Link>
+                                <Nav.Link as={Link} to="/Contact">Contact</Nav.Link>
                                 <NavDropdown title="Links" id="basic-nav-dropdown">
                                     <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                     <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -25,10 +28,12 @@ export default function App() {
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
-            </div>
-            <div>
-                <Home></Home>
-            </div>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/About" element={<About />}/>
+                    <Route path="/Contact" element={<Contact />}/>
+                </Routes>
+            </Router>                        
         </div>
     )
 }
