84b92d473b901a121b837a08009e92597b6b69cb
ivanPepe <ji.perez@dataware.com.mx>
2023-06-26 12:37:57 -0600

    Projects agregados , scroll personalizado , terminacion de proyecto base

--- portafolioIvan/src/Components/Projects/Project.jsx
+++ portafolioIvan/src/Components/Projects/Project.jsx
@@ -1,56 +1,46 @@
 import './proyect.css'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+// importacion de las imagenes
+import p1 from './p1.gif'
+import { projectData } from '../../features/datas/projectsDatas';
 
-export default function Project(){
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-  
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
-  return ( 
+
+export default function Project() {
+
+  return (
     <section className="project-section" id='project'>
       <h1>Projects</h1>
-      <TableContainer component={Paper} >
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow className='project-table'>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <div className="project-datas">        
+        {
+          projectData.map(
+            data => (
+              <Card sx={{ maxWidth: 345 }} className='project-cards'>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={data.img}
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {data.technology} / {data.name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {data.info}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <a target="_blank" href={data.url}><Button size="small">Go</Button></a>
+          </CardActions>
+        </Card>
+            )
+          )
+        }
+      </div>
     </section>
   )
 }
---
+++ portafolioIvan/src/features/datas/projectsDatas.jsx
@@ -0,0 +1,9 @@
+import p1 from '../../assets/p1.gif';
+import p2 from '../../assets/p2.png';
+import p3 from '../../assets/p3.png';
+
+export const projectData = [
+  { info : "Web API with Rick and Morty API" , technology : "Reacj.JS" ,name:"Rcik & Morty", url : "https://ivanisc95.github.io/rick-and-morty-characters/" ,img : p1},
+  { info : "Contact page of CPGE Company" , technology : "React.JS" , name: "CPGE Company",url : "https://ivanisc95.github.io/cpge/" ,img : p2},
+  { info : "Web application for loan management with LocalStorage" ,technology : "Angular",name: "Web application" ,url : "https://ivanisc95.github.io/MiEconomia-app/", img : p3}
+]
