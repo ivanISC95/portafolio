68e12577921734d79e9b2d9d767a004e387549a6
ivanPepe <ji.perez@dataware.com.mx>
2023-06-27 12:10:01 -0600

    feat(contact) : add location

--- portafolioIvan/src/Components/Contact/Contact.jsx
+++ portafolioIvan/src/Components/Contact/Contact.jsx
@@ -2,7 +2,7 @@ import './contact.css'
 import { AiFillLinkedin, AiOutlineMail, AiFillGithub } from 'react-icons/ai'
 import { BsFileEarmarkArrowDown } from 'react-icons/bs'
 import IconButton from '@mui/material/IconButton';
-//import { cv } from '../../cv/Perez Ugalde Jose Ivan CV.pdf'
+import { CiLocationOn } from 'react-icons/ci'
 import cv from '../../cv/Perez Ugalde Jose Ivan CV.pdf'
 
 export default function Contact() {
@@ -23,8 +23,15 @@ export default function Contact() {
         <div className="card">
         <IconButton color="primary" className="card" href={cv} target="_blank">
           <BsFileEarmarkArrowDown className="iconsSkills"/>
+          <p>Download my resume</p>
         </IconButton>
         </div>      
+        <div className="card">
+        <IconButton color="primary" className="card" href='https://www.google.com.mx/maps/place/San+Juan+del+R%C3%ADo,+Qro./@20.4033863,-100.0146718,13z/data=!3m1!4b1!4m6!3m5!1s0x85d30b6be9806eb5:0xfabddb9e7679cd43!8m2!3d20.3951106!4d-99.9856344!16zL20vMDhkZnc2?entry=ttu' target="_blank">
+          <CiLocationOn className="iconsSkills"/>
+          <p>San Juan del Rio / Queretaro / Mexico</p>
+        </IconButton>
+        </div> 
       </div>
       <br />
     </section>
