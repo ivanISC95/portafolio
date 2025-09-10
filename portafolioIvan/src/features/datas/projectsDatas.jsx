337a2bae63a44e196196c57003df69f81c67b415
Jose Ivan Perez <ivan2295isc@gmail.com>
2025-09-09 22:48:58 -0600

    Update Projects
    Update projects, add coolview telemetry
    Update icons

--- portafolioIvan/src/features/datas/projectsDatas.jsx
+++ portafolioIvan/src/features/datas/projectsDatas.jsx
@@ -1,9 +1,11 @@
-import p1 from '../../assets/p1.gif';
-import p2 from '../../assets/p2.png';
-import p3 from '../../assets/p3.png';
+import Coolview_Graphs from '../../assets/Coolview/Coolview_Graphs.webp';
+import morty from '../../assets/Rick&Morty/Rick&Morty.webp';
+import management_loans from '../../assets/ProjectMoneyAngular/Money.webp';
+
+
 
 export const projectData = [
-  { info : "Web API with Rick and Morty API" , technology : "Reacj.JS" ,name:"Rcik & Morty", url : "https://ivanisc95.github.io/rick-and-morty-characters/" ,img : p1},
-  { info : "Contact page of CPGE Company" , technology : "React.JS" , name: "CPGE Company",url : "https://ivanisc95.github.io/cpge/" ,img : p2},
-  { info : "Web application for loan management with LocalStorage" ,technology : "Angular",name: "Web application" ,url : "https://ivanisc95.github.io/MiEconomia-app/", img : p3}
+  { info : "Real-time telemetry visualization showing the current status of a coolers , built with Angular, Plotly, and FastAPI. Enables preventive maintenance and targeted repairs, reducing costs and increasing revenue" ,technology : "React",name: "Coolview Telemetry" ,url : "https://solkos-coolview-2-qa.firebaseapp.com/Coolview/F94240101093", img : Coolview_Graphs},
+  { info : "Web API with Rick and Morty API" , technology : "Reacj.JS" ,name:"Rcik & Morty", url : "https://ivanisc95.github.io/rick-and-morty-characters/" ,img : morty},  
+  { info : "Web application for loan management with LocalStorage" ,technology : "Angular",name: "Web application" ,url : "https://ivanisc95.github.io/MiEconomia-app/", img : management_loans},
 ]
